2a9959d3294ac57fde9336295d23ca5e129d1703
reginafcompton <reginafcompton@gmail.com>
2016-05-24 20:10:12 +0200

    Add new form for participants

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -8,4 +8,27 @@ class ParticipantsController < ApplicationController
     @participant = Participant.find(params[:id])
   end
 
+  def new
+    @participant = Participant.new
+    @days = Day.all.map { |d| [d.day, d.id] }
+    @cities = City.all.map{ |c| [c.name, c.id] }
+  end
+
+  def create
+    @participant = Participant.new(participant_params)
+
+    if @participant.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+
+  end
+
+  def participant_params
+      params.require(:participant).permit(:name, :arrival_time, :arrival_day_id, :arrival_city_id,  :arrival_mode,  :departure_time, :departure_day_id, :departure_city_id, :departure_mode)
+    end
+
 end
+
+
